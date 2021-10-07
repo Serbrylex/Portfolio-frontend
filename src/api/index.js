a827e0ea77d0d381b33ed3122cb374fa6572cbf2
Serbrylex <sbryanmadridn@gmail.com>
2021-10-07 13:50:08 -0500

    Producción, almost ready

--- src/api/index.js
+++ src/api/index.js
@@ -16,38 +16,41 @@
 	}
 
 }*/
+import comparteride from '../images/comparteride.png'
+import instagram from  '../images/instagram.png'
+import portfolio from '../images/MyPortafolio.png'
 
 const apiCall = {
 	proyects: [
 		{
 			'name': 'Comparte Ride',
-			'image': '/static/media/yomero.053db553.jpeg',
+			'image': comparteride,
 			'date': new Date('2021', '08', '30'),
-			'description': 'Comparte Ride es una aplicación que permite conectar individuos con necesidades en común.',
+			'description': 'Comparte Ride es una aplicación que permite conectar individuos con necesidades en común. (Aún está en progreso)',
 			'gols': 'El principal desafío de está aplicación es que tube que sumergirme en python hasta sus bases para poder seguir el desarrollo del backend y lograr desarrollar una api que es consumida en el frontend, lo más interesante es que implementa tokens en el envio de peticiones, por lo que tube que aprender sobre JWT.',
-			'tools': ['Django', 'React.js', 'Celery', 'Redis', 'Docker'],
-			'github': 'github.com/Serbrylex',
-			'link': 'htts://www.github.com/Serbrylex'
+			'tools': ['Django', 'React.js', 'Celery', 'Redis', 'Docker', 'AWS'],
+			'github': 'https://github.com/Serbrylex/cride_frontend',
+			'link': 'https://master.d39un5rth4eij9.amplifyapp.com/'
 		},
 		{
 			'name': 'Original Instagram',
-			'image': '/static/media/yomero.053db553.jpeg',
+			'image': instagram,
 			'date': new Date('2021', '06', '30'),
 			'description': 'Es un clon de instagram.',
-			'gols': 'Está aplicación fue diertido desarrollarla porque para las bases seguí dos cursos y ambos dejaban bastante a pulir, por lo que tube que aprender ciertas cosas por mi cuenta, implementando todos los eventos que utilizan peticiones al backend haciendo uso de fetch.',
-			'tools': ['Django', 'React.js'],
-			'github': 'github.com/Serbrylex',
-			'link': 'htts://www.github.com/Serbrylex',
+			'gols': 'Está aplicación fue diertido desarrollarla porque para las bases seguí dos cursos y ambos dejaban bastante a pulir, en ambos cursos se construia un monolito, por lo que tube que aprender ciertas cosas por mi cuenta, implemente el backend como una API a la que se comunica el frontend.',
+			'tools': ['Django', 'React.js', 'AWS Amplify', 'Heroku'],
+			'github': 'https://github.com/Serbrylex/instagram_for_my_portfolio',
+			'link': 'https://master.d2wckr9gxsnzjm.amplifyapp.com/',
 		},
 		{
 			'name': 'Mi sitio web',
-			'image': '/static/media/yomero.053db553.jpeg',
+			'image': portfolio,
 			'date': new Date('2021', '09', '3'),
-			'description': 'Es mi sitio web, tiene una descripción de mis proyectos, enlaces a ellos, un pequeño blog donde pienso escribir semanalmente y cada vez que aprenda algo interesante.',
-			'gols': 'El principal desafío de está aplicación fue que no segui ningun curso por primera vez, tenía que seguir el mapa de lo que ya sabía.',
-			'tools': ['Django', 'React.js'],
-			'github': 'github.com/Serbrylex',
-			'link': 'htts://www.github.com/Serbrylex'
+			'description': 'Es mi sitio web, tiene una descripción de mis proyectos, enlaces a ellos, y proximamente estare trabajando en un pequeño blog donde pienso escribir semanalmente y cada vez que aprenda algo interesante.',
+			'gols': 'El principal desafío de está aplicación fue que no seguí ningun curso por primera vez, tenía que seguir el mapa de lo que ya sabía.',
+			'tools': ['React.js', 'AWS Amplify'],
+			'github': 'https://github.com/Serbrylex/Portfolio-frontend',
+			'link': 'https://us-east-2.console.aws.amazon.com/amplify/home?region=us-east-2&code=e62aa8262a25ec12c33e#/dpk3kg6kahbq9'
 		},		
 	]	
 }
